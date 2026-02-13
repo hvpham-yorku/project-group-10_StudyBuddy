157c97c799733003ee987cb35055b7e2f8f86495
Vaughn Chan <vaughnchan8@gmail.com>
2026-02-12 19:47:57 -0500

    Fix CORS issue
    Issue caused by hard-coded path using 'localhost'

--- Frontend/index.jsx
+++ Frontend/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 function App() {
     const[data, setData] = useState(null)
     useEffect(() => {
-	fetch('http://localhost:8080/api/studentcontroller/getstudents')
+	fetch('/api/studentcontroller/getstudents')
 	    .then(res => res.json())
 	    .then(json => setData(json));
     }, []);
