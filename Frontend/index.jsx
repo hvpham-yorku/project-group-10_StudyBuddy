76fe4db5a2f1ece3868632255c0fc59a1527f20f
Vaughn Chan <vaughnchan8@gmail.com>
2026-02-13 19:12:05 -0500

    Convert regular html files to React files

--- Frontend/index.jsx
+++ Frontend/index.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./style.css";
 
+/* The Pages */
 import Home from "./pages/Home";
 import StudentProfile from "./pages/StudentProfile";
 import EventsPage from "./pages/Events";
+import SignInPage from "./pages/SignIn";
+
 
 function App() {
   return (
@@ -13,7 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<StudentProfile />} />
-		<Route path="/events" element={<EventsPage/>} />
+		    <Route path="/events" element={<EventsPage/>} />
+        <Route path="/signin" element={<SignInPage />} />
       </Routes>
     </BrowserRouter>
   );
