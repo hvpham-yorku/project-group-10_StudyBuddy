4fcbcae5a4fb7dad0ab54d1c2580ffd71e5566cf
Vaughn Chan <vaughnchan8@gmail.com>
2026-02-13 19:39:33 -0500

    Integrate html home to react page

--- Frontend/pages/Home.jsx
+++ Frontend/pages/Home.jsx
@@ -4,11 +4,19 @@ import TopBar from "../components/TopBar";
 export default function Home() {
   return (
     <div>
-      <TopBar />
-      <div style={{ padding: "20px" }}>
-        <h1>Home</h1>
-        <p>This is the home page. Click the profile icon top-right.</p>
-      </div>
+      <TopBar/>
+
+      <div className="container">
+        <div className="left-side">
+            <div className="logo">
+                <img src="shrunken_logo.webp" alt="Study Buddy Logo"/>
+            </div>
+          </div>
+
+          <div className="right-side">
+            This is the home page. Click the profile icon top-right.
+          </div>
+        </div>
     </div>
   );
 }
