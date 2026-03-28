211f0c3493c7ce7e20de27f5c730e4b335e679fe
Vaughn Chan <vaughnchan8@gmail.com>
2026-03-28 19:11:51 -0400

    Use mock email for EmailServiceIntegrationTests.java

--- StudyBuddy/src/test/java/ca/yorku/my/StudyBuddy/integration/EmailServiceIntegrationTests.java
+++ StudyBuddy/src/test/java/ca/yorku/my/StudyBuddy/integration/EmailServiceIntegrationTests.java
@@ -7,12 +7,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.Session;
+import org.junit.jupiter.api.BeforeEach;
+import static org.mockito.Mockito.when;
+
 import ca.yorku.my.StudyBuddy.StudyBuddyApplication;
 
 @ActiveProfiles("stub")
 @SpringBootTest(classes = StudyBuddyApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class EmailServiceIntegrationTests {
+    
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @BeforeEach
+    void setupMock() {
+        when(javaMailSender.createMimeMessage()).thenReturn(new MimeMessage((Session) null));
+    }
+
+    
     @Autowired
     private MockMvc mockMvc;
 
